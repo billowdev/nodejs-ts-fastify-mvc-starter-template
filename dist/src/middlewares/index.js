bee427b1326212bef14bf0cd5bc322c1a6764aaa
Akkarapon Phikulsri <lacakp.contact@gmail.com>
2022-07-11 12:09:28 +0700

    .prettierrc.ts

--- dist/src/middlewares/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.protectedRoutes = exports.verifyToken = void 0;
-const auth_middleware_1 = require("./auth.middleware");
-Object.defineProperty(exports, "verifyToken", { enumerable: true, get: function () { return auth_middleware_1.verifyToken; } });
-const protectedRoutes_middleware_1 = require("./protectedRoutes.middleware");
-Object.defineProperty(exports, "protectedRoutes", { enumerable: true, get: function () { return protectedRoutes_middleware_1.protectedRoutes; } });
